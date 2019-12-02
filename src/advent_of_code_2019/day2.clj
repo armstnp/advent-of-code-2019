d3e01f433799e15193c10ec809f796d233f2fcc0
Nathan Armstrong <kenogu.labz@gmail.com>
2019-12-02 02:03:16 -0600

    Solve day 2.

--- src/advent_of_code_2019/day2.clj
+++ src/advent_of_code_2019/day2.clj
@@ -1,18 +1,49 @@
-;; Set NS before starting: C-c M-n n
 (ns advent-of-code-2019.day2
   (:require [advent-of-code-2019.core :as core]
-            [advent-of-code-2019.left-shark :as ls]
-            [clojure.string :as str]
-            [clojure.math.combinatorics :as combo]
-            [is-prime.core :refer [is-prime]]))
-;;  (:import  [org.jgrapht.graph SimpleDirectedWeightedGraph Pseudograph DefaultEdge DefaultWeightedEdge]))
+            [clojure.string :as str]))
 
-(def input (->> "day2.txt" core/read-input str/split-lines))
+(def input (-> "day2.txt"
+               core/read-input
+               str/trim
+               (str/split #",")
+               (->> (mapv core/parse-int))))
 
-(defn parse-line
-  [s]
-  (-> s
-      :components
-      (ls/parse
-       ;; Fill in parse components here
-       )))
+(defn run-instr
+  [{:keys [memory ip] :as computer}]
+  (let [[op in-addr-1 in-addr-2 out-addr] (take 4 (drop ip memory))
+        memory' (case op
+                  1 (assoc memory out-addr (+ (memory in-addr-1) (memory in-addr-2)))
+                  2 (assoc memory out-addr (* (memory in-addr-1) (memory in-addr-2)))
+                  99 memory
+                  (str "Unknown op-code " op))
+        ip' (+ ip 4)]
+    (if (= op 99)
+      (assoc computer :halt true)
+      (assoc computer :memory memory' :ip ip'))))
+
+(defn run-program
+  [program]
+  ((->> {:memory program :ip 0}
+       (iterate run-instr)
+       (drop-while #(not (:halt %)))
+       first
+       :memory)
+   0))
+
+(defn run-noun-verb
+  [program noun verb]
+  (-> program
+      (assoc 1 noun)
+      (assoc 2 verb)
+      run-program))
+
+(run-noun-verb input 12 2)
+
+(def noun-verb-combo (fn [[noun verb]] (+ (* 100 noun) verb)))
+
+(-> (for [noun (range 100)
+          verb (range 100)
+          :when (= (run-noun-verb input noun verb) 19690720)]
+      [noun verb])
+    first
+    noun-verb-combo)
