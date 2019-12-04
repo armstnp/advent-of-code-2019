76154e08ee41477b699c42cdca7fb513a67e4720
Nathan Armstrong <kenogu.labz@gmail.com>
2019-12-04 00:49:47 -0600

    Solve day 4.

--- src/advent_of_code_2019/day4.clj
+++ src/advent_of_code_2019/day4.clj
@@ -1,18 +1,34 @@
-;; Set NS before starting: C-c M-n n
 (ns advent-of-code-2019.day4
-  (:require [advent-of-code-2019.core :as core]
-            [advent-of-code-2019.left-shark :as ls]
-            [clojure.string :as str]
-            [clojure.math.combinatorics :as combo]
-            [is-prime.core :refer [is-prime]]))
-;;  (:import  [org.jgrapht.graph SimpleDirectedWeightedGraph Pseudograph DefaultEdge DefaultWeightedEdge]))
+  (:require [advent-of-code-2019.core :as core]))
 
-(def input (->> "day4.txt" core/read-input str/split-lines))
+(def input-min 172930)
+(def input-max 683082)
 
-(defn parse-line
-  [s]
-  (-> s
-      :components
-      (ls/parse
-       ;; Fill in parse components here
-       )))
+(defn two-adjacent?
+  [n]
+  (re-find #"(\d)(?:\1)" (str n)))
+
+(defn not-decreasing?
+  [n]
+  (let [s (seq (str n))]
+    (= s (sort s))))
+
+(def password-p1? (every-pred two-adjacent? not-decreasing?))
+
+(->> input-max
+     inc
+     (range input-min)
+     (filter password-p1-re?)
+     count)
+
+(defn exactly-two-adjacent?
+  [n]
+  (re-find #"^(\d)(?:\1)(?!\1)|(\d)(?!\4)(\d)(?:\5)(?!\5)" (str n)))
+
+(def password-p2? (every-pred exactly-two-adjacent? not-decreasing?))
+
+(->> input-max
+     inc
+     (range input-min)
+     (filter password-p2?)
+     count)
