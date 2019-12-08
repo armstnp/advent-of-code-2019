ac1abb65c1a78373e25f98832de6be86d7161166
Nathan Armstrong <kenogu.labz@gmail.com>
2019-12-08 00:29:24 -0600

    Solve day 8.

--- src/advent_of_code_2019/day8.clj
+++ src/advent_of_code_2019/day8.clj
@@ -1,18 +1,37 @@
-;; Set NS before starting: C-c M-n n
 (ns advent-of-code-2019.day8
   (:require [advent-of-code-2019.core :as core]
-            [advent-of-code-2019.left-shark :as ls]
-            [clojure.string :as str]
-            [clojure.math.combinatorics :as combo]
-            [is-prime.core :refer [is-prime]]))
-;;  (:import  [org.jgrapht.graph SimpleDirectedWeightedGraph Pseudograph DefaultEdge DefaultWeightedEdge]))
+            [clojure.string :as str]))
 
-(def input (->> "day8.txt" core/read-input str/split-lines))
+(def input (-> "day8.txt" core/read-input str/trim))
+(def width 25)
+(def height 6)
+(def layer-area (* width height))
 
-(defn parse-line
-  [s]
-  (-> s
-      :components
-      (ls/parse
-       ;; Fill in parse components here
-       )))
+(defn num-layers
+  [input]
+  (/ (count input) layer-area))
+
+(defn count-char
+  [c layer]
+  (count (filter (partial = c) layer)))
+
+(->> input
+     (partition layer-area)
+     (apply min-key (partial count-char \0))
+     ((juxt (partial count-char \1) (partial count-char \2)))
+     (apply *))
+
+(defn mix
+  [c1 c2]
+  (if (= \2 c1)
+    c2
+    c1))
+
+(->> input
+     (partition layer-area)
+     (reduce (partial map mix))
+     (map #(if (= \0 %) \space %))
+     (partition width)
+     (map (comp (partial apply str) vec))
+     (str/join "\n")
+     println)
