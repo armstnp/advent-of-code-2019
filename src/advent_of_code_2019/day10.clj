8505ac7a543cf1282e9e65c8342a933ad2df129f
Nathan Armstrong <kenogu.labz@gmail.com>
2019-12-10 01:14:07 -0600

    Solve day 10.

--- src/advent_of_code_2019/day10.clj
+++ src/advent_of_code_2019/day10.clj
@@ -1,18 +1,67 @@
-;; Set NS before starting: C-c M-n n
 (ns advent-of-code-2019.day10
   (:require [advent-of-code-2019.core :as core]
-            [advent-of-code-2019.left-shark :as ls]
-            [clojure.string :as str]
-            [clojure.math.combinatorics :as combo]
-            [is-prime.core :refer [is-prime]]))
-;;  (:import  [org.jgrapht.graph SimpleDirectedWeightedGraph Pseudograph DefaultEdge DefaultWeightedEdge]))
+            [clojure.string :as str]))
 
-(def input (->> "day10.txt" core/read-input str/split-lines))
+(def input (->> "day10.txt" core/read-input str/split-lines (mapv vec)))
 
-(defn parse-line
-  [s]
-  (-> s
-      :components
-      (ls/parse
-       ;; Fill in parse components here
-       )))
+(defn width
+  [input]
+  (count (first input)))
+(defn height
+  [input]
+  (count input))
+
+(defn map->asteroids
+  [input]
+  (for [x (range 0 (width input))
+        y (range 0 (height input))
+        :when (= \# ((input y) x))]
+    [x y]))
+
+(defn best-candidate
+  [input]
+  (let [asteroids (map->asteroids input)
+        asteroid-lines (for [station asteroids
+                             target asteroids
+                             :when (not= station target)]
+                         [station (:theta (polar station target))])]
+    (->> asteroid-lines
+         distinct
+         (map first)
+         frequencies
+         (apply max-key second))))
+
+(second (best-candidate input))
+
+(def station (first (best-candidate input)))
+
+(defn polar
+  [[sx sy :as source] [tx ty :as target]]
+  {:r (apply + (map (comp #(Math/abs %) -) source target)) ;; Close enough for our purposes
+   :theta (- Math/PI (apply #(Math/atan2 %1 %2) ^:double (map (comp double -) target source)))
+   :asteroid target})
+
+(defn polar-lines
+  [input station]
+  (let [asteroids (map->asteroids input)
+        polars (for [target asteroids
+                     :when (not= station target)]
+                 (polar station target))]
+    (->> polars
+         (group-by :theta)
+         (sort-by first)
+         (map (comp (partial map :asteroid) (partial sort-by :r) second)))))
+
+(defn laser-until
+  [[[next-asteroid & rest-asteroids] & rest-lines] n]
+  (if (= n 1)
+    next-asteroid
+    (let [line' (if (empty? rest-asteroids) [] [rest-asteroids])
+          lines' (concat rest-lines line')]
+      (recur lines' (dec n)))))
+
+
+(let [input input]
+  (-> input
+      (polar-lines station)
+      (laser-until 200)))
