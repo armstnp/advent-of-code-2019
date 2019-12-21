68e21174394d8b0e14433f9f249e995c10ac6d67
Nathan Armstrong <kenogu.labz@gmail.com>
2019-12-20 23:47:43 -0600

    Remove cruft from day 21.

--- src/advent_of_code_2019/day21.clj
+++ src/advent_of_code_2019/day21.clj
@@ -1,11 +1,6 @@
-;; Set NS before starting: C-c M-n n
 (ns advent-of-code-2019.day21
   (:require [advent-of-code-2019.core :as core]
-            [advent-of-code-2019.left-shark :as ls]
-            [clojure.string :as str]
-            [clojure.math.combinatorics :as combo]
-            [is-prime.core :refer [is-prime]]))
-;;  (:import  [org.jgrapht.graph SimpleDirectedWeightedGraph Pseudograph DefaultEdge DefaultWeightedEdge]))
+            [clojure.string :as str]))
 
 (def input (-> "day21.txt"
                core/read-input
